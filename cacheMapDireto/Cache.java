8df20437c3cd8252031b32ba23d87f5892113853
Marco Aurélio Silva de Souza Júnior <aureliojuniorcmrj@hotmail.com>
2022-09-20 14:09:47 -0300

    Update Cache.java

--- cacheMapDireto/Cache.java
+++ cacheMapDireto/Cache.java
@@ -4,6 +4,7 @@ public class Cache extends Memoria {
     private RAM ram;
 
     private int inicio;
+    /* int[r][w+1] , int[r][0] = t */
     private int[][] dados;
     private boolean modificada = false;
 
@@ -16,21 +17,28 @@ public class Cache extends Memoria {
 
     @Override
     public int Read(int enderecoBit) throws EnderecoInvalido {
+        System.out.println("READCACHE");
         int resp;
         int x = enderecoBit;
+        System.out.println("x="+Integer.toBinaryString(x));
         int t, r, s, w;
 
         // extrair t, r e w
         // se cache line = 2^6 palavras, w possui 6 bits 
         w = 0b111_1111 & x;
+        System.out.println("w="+Integer.toBinaryString(w));
         // se cache possui 2^12 cache lines, r possui 12 bits
         r = (x >> 6) & 0b1111_1111_1111;
-        // capacidade = 8M, x é de 23 bits, t possui 5 bits 
+        System.out.println("r="+Integer.toBinaryString(r));
+        // capacidade = 8M = 2^23, x é de 23 bits, t possui 5 bits 
         t = (x >> 18) & 0b1_1111;
+        System.out.println("t="+Integer.toBinaryString(t));
         // tag da posicao r da cache
         int t_ = (dados[r][0] >> 18) & 0b1_1111;
+        System.out.println("t_="+Integer.toBinaryString(t_));
         // s = concatenação t+r
         s = t_ | r | 0b00_0000;
+        System.out.println("s="+Integer.toBinaryString(s));
 
         // comparar t com t'(cacheline)
         boolean cacheHit = t==t_;
@@ -53,14 +61,58 @@ public class Cache extends Memoria {
         }
 
         // a palavra no endereço solicitado é retornada à CPU
+        System.out.println("resp="+resp);
         return resp;
     }
 
-
     @Override
-    public void Write(int endereco, int valor) {
+    public void Write(int enderecoBit, int valor) throws EnderecoInvalido {
+        int x = enderecoBit;
+        System.out.println("x="+Integer.toBinaryString(x));
+        int t, r, s, w;
+        
+        System.out.println("WRITE");
+        
+        // extrair t, r e w
+        // se cache line = 2^6 palavras, w possui 6 bits 
+        w = 0b111_1111 & x;
+        System.out.println("w="+Integer.toBinaryString(w));
+        // se cache possui 2^12 cache lines, r possui 12 bits
+        r = (x >> 6) & 0b1111_1111_1111;
+        System.out.println("r="+Integer.toBinaryString(r));
+        // capacidade = 8M = 2^23, x é de 23 bits, t possui 5 bits 
+        t = (x >> 18) & 0b1_1111;
+        System.out.println("t="+Integer.toBinaryString(t));
+        // tag da posicao r da cache
+        int t_ = (dados[r][0] >> 18) & 0b1_1111;
+        System.out.println("t_="+Integer.toBinaryString(t_));
+        // s = concatenação t+r
+        s = t_ | r | 0b00_0000;
+        System.out.println("s="+Integer.toBinaryString(s));
+
+        // comparar t com t'(cacheline)
+        boolean cacheHit = t==t_;
+        if(cacheHit) {
+            // cache hit: utiliza-se w para retornar à CPU a palavra na posição w
+            System.out.println("CACHEHIT");
+        }
+        else {
+            // cache miss:
+            System.out.println("CacheMiss - End: " + x);
+            // cache line r corresponde a um outro bloco de memória
+            if(modificada) {
+                // Se esta cache line tiver sido alterada, ela é copiada para a memória principal
+                // a partir do endereço s
+                copyLineToRAM(t_, r);
+                modificada = false;
+            }
+            // o bloco s da memória principal é trazido para a cache line
+            dados[r][w+1] = valor; /* +1 pois a posicao 0 eh da tag */
+            //resp = dados[r][w+1]; /* +1 pois a posicao 0 eh da tag */
+        }
 
     }
+
 
 
     private void copyLineToRAM(int t_, int r) throws EnderecoInvalido {
