d86c96659f2c8365b978a0f6adb8615d4858c74f
Marco Aurélio Silva de Souza Júnior <aureliojuniorcmrj@hotmail.com>
2022-09-20 16:09:32 -0300

    Update Main.java

--- cacheMapDireto/Main.java
+++ cacheMapDireto/Main.java
@@ -12,14 +12,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("* Arquitetura von Neumann Básica - PSCF\n");
-        
-        // cria componentes da arquitetura
-        IO io = new IO(System.out);
-        RAM ram = new RAM(8_388_608); // 8M
-        Cache cache = new Cache(4096, 64, ram);
-        CPU cpu = new CPU(io, cache);
-        
         try {
+            // cria componentes da arquitetura
+            IO io = new IO(System.out);
+            RAM ram = new RAM(8_388_608); // 8M - 131.072 blocos de 64
+            Cache cache = new Cache(4096, 64, ram);
+            CPU cpu = new CPU(io, cache);
             // carrega "programa" na memória
             final int inicio = 0b00001111;
             ram.Write(inicio, 118);
